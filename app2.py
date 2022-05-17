f014cd65fce30ebd307b08ee243cbd9baecbc96a
P3t3rtm <33187689+P3t3rtm@users.noreply.github.com>
2022-05-16 20:55:39 -0600

    why doesnt it work??

---
+++ app2.py
@@ -0,0 +1,11 @@
+
+import pyvips
+
+# image = pyvips.Image.new_from_file('1.png', access='sequential')
+# image *= [1, 2, 1]
+# mask = pyvips.Image.new_from_array([[-1, -1, -1],
+#                                     [-1, 16, -1],
+#                                     [-1, -1, -1]
+#                                    ], scale=8)
+# image = image.conv(mask, precision='integer')
+# image.write_to_file('x.jpg')
